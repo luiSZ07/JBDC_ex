ad0c0867c26ab9ed796f55bf9664c54cb357ebce
luiSZ07 <luiz.miguel0705@gmail.com>
2023-09-23 14:31:04 -0400

    fix:

--- banco-java/src/main/java/ufmt/biblioteca/Main.java
+++ banco-java/src/main/java/ufmt/biblioteca/Main.java
@@ -6,37 +6,32 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
-
 public class Main {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/db_biblioteca";
         String user = "luiz";
         String password = "luiz1234";
         Connection conexao;
-        try{
+        try {
             conexao = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao conectar" + e.getMessage(), e);
         }
-        catch(SQLException e){
-            throw new RuntimeException("Erro ao conectar"+e.getMessage(), e);
-        }
-        
+
         String sql = "SELECT * FROM livro";
 
-        try{
+        try {
             PreparedStatement consulta = conexao.prepareStatement(sql);
             ResultSet resultado = consulta.executeQuery();
-            while(resultado.next()){
+            while (resultado.next()) {
                 System.out.println("ID: " + resultado.getInt("id_livro"));
                 System.out.println("ISBN: " + resultado.getInt("isbn"));
                 System.out.println("NOME: " + resultado.getString("nome_livro"));
                 System.out.println("ID DO AUTOR: " + resultado.getInt("id_autor"));
                 System.out.println("DATA DE PUBLICAÇÃO: " + resultado.getDate("data_publicacao"));
                 System.out.println("PREÇO: " + resultado.getFloat("preco_livro"));
-    
             }
-        }
-        catch(SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException("Erro ao consultar" + e.getMessage(), e);
         }
     }
